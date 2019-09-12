a310d363f6fc67812bc01d936aaf5b3f7c50fb4e
nathanhung <nathanhung8@gmail.com>
2019-09-12 00:10:05 -0400

    Added a0

---
+++ a0/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        System.out.println("--> Java JDK version: " + version);
+    }
+}
