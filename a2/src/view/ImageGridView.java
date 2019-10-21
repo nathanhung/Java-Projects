aca81b6928ec4ee717b4daec725d5f771fe443e5
nathanhung <nathanhung8@gmail.com>
2019-10-21 16:47:03 -0400

    final push

--- a2/src/view/ImageGridView.java
+++ a2/src/view/ImageGridView.java
@@ -26,35 +26,10 @@ public class ImageGridView extends JPanel implements IView {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         //this.setSize(100,100);
         this.setPreferredSize(new Dimension(350,350));
-        this.setMaximumSize(new Dimension(400,350));
-        this.setMinimumSize(new Dimension(350,350));
+        this.setMaximumSize(new Dimension(400,450));
+        this.setMinimumSize(new Dimension(350,450));
 
         try {
-            /*
-            // add image icon (IMPLEMENTATION 1)
-            File input = new File(this.imageModel.filePath.toString());
-            BufferedImage buffImage = ImageIO.read(input);
-            BufferedImage resized = resize(buffImage, 200, 200);
-            String resizeImgFilePath = this.imageModel.filePath.toString();
-            //System.out.println(resizeImgFilePath);
-            String fileExtension = "";
-            int i = this.imageModel.filePath.toString().lastIndexOf('.');
-            if (i > 0) {
-                fileExtension = this.imageModel.filePath.toString().substring(i + 1);
-                //System.out.println(fileExtension);
-            }
-            File output = new File(resizeImgFilePath.substring(0,i)
-                    + "-resized-" + "." + fileExtension);
-            ImageIO.write(resized, "png", output);
-
-            ImageIcon scaledImage = new ImageIcon(resizeImgFilePath.substring(0,i)
-                    + "-resized-" + "." + fileExtension);
-            //System.out.println(resizeImgFilePath.substring(0,i) + "-resized-" + "." + fileExtension);
-            JLabel image = new JLabel(scaledImage);
-            this.add(image);
-
-
-             */
             // add image icon (IMPLEMENTATION 2)
             ImageIcon originalImage = new ImageIcon(this.imageModel.filePath.toString());
 
@@ -65,12 +40,7 @@ public class ImageGridView extends JPanel implements IView {
             //System.out.println(originalWidth);
             //System.out.println(originalHeight);
 
-            Dimension scaledDimension = getScaledDimension(new Dimension(originalHeight, originalWidth),
-                    new Dimension(200,200));
-
-            //System.out.println(scaledDimension.width);
-            //System.out.println(scaledDimension.height);
-            Image scaled = originalImage.getImage().getScaledInstance(scaledDimension.width, scaledDimension.height, Image.SCALE_SMOOTH);
+            Image scaled = originalImage.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
             ImageIcon scaledImage = new ImageIcon(scaled);
             JLabel image = new JLabel(scaledImage);
             this.add(image);
@@ -116,33 +86,6 @@ public class ImageGridView extends JPanel implements IView {
             }
         });
         this.add(clear_button);
-    }
-
-    // used for scaling image
-    private static BufferedImage resize(BufferedImage img, int height, int width) {
-        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = resized.createGraphics();
-        g2d.drawImage(tmp, 0, 0, null);
-        g2d.dispose();
-        return resized;
-    }
-
-    // used for scaling image
-    public static Dimension getScaledDimension(Dimension originalDim, Dimension boundary) {
-        int newHeight = originalDim.height;
-        int newWidth = originalDim.width;
-
-        if (newHeight > boundary.height) {
-            newHeight = boundary.height;
-            newWidth = (newHeight * originalDim.width) / originalDim.height;
-        }
-
-        if (originalDim.width > boundary.width) {
-            newWidth = boundary.width;
-            newHeight = (newWidth * originalDim.height) / originalDim.width;
-        }
-        return new Dimension(newWidth, newHeight);
     }
 
     public void updateView() {
--- a2/src/view/ImageListView.java
+++ a2/src/view/ImageListView.java
@@ -33,46 +33,13 @@ public class ImageListView extends JPanel implements IView {
         JPanel image_panel = new JPanel();
         // add image:
         try {
-            /*
-            File input = new File(this.imageModel.filePath.toString());
-            BufferedImage buffImage = ImageIO.read(input);
-            BufferedImage resized = resize(buffImage, 200, 200);
-            String resizeImgFilePath = this.imageModel.filePath.toString();
-            //System.out.println(resizeImgFilePath);
-            String fileExtension = "";
-            int i = this.imageModel.filePath.toString().lastIndexOf('.');
-            if (i > 0) {
-                fileExtension = this.imageModel.filePath.toString().substring(i + 1);
-                //System.out.println(fileExtension);
-            }
-            File output = new File(resizeImgFilePath.substring(0,i)
-                    + "-resized-" + "." + fileExtension);
-            ImageIO.write(resized, "png", output);
-
-            ImageIcon scaledImage = new ImageIcon(resizeImgFilePath.substring(0,i)
-                    + "-resized-" + "." + fileExtension);
-            //System.out.println(resizeImgFilePath.substring(0,i) + "-resized-" + "." + fileExtension);
-            JLabel image = new JLabel(scaledImage);
-
-             */
             // add image icon (IMPLEMENTATION 2)
             ImageIcon originalImage = new ImageIcon(this.imageModel.filePath.toString());
 
             int originalWidth = originalImage.getIconWidth();
             int originalHeight = originalImage.getIconHeight();
-            //System.out.println(originalWidth);
-            //System.out.println(originalHeight);
 
-            Dimension scaledDimension = getScaledDimension(new Dimension(originalHeight, originalWidth),
-                    new Dimension(200,200));
-            //int scaledWidth = originalWidth / (originalWidth / 250);
-            //int scaledHeight = originalHeight / (originalHeight / 250);
-
-            //System.out.println(scaledDimension.width);
-            //System.out.println(scaledDimension.height);
-            Image scaled = originalImage.getImage().getScaledInstance(scaledDimension.width, scaledDimension.height, Image.SCALE_SMOOTH);
-
-
+            Image scaled = originalImage.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
             ImageIcon scaledImage = new ImageIcon(scaled);
             JLabel image = new JLabel(scaledImage);
 
@@ -127,38 +94,7 @@ public class ImageListView extends JPanel implements IView {
         this.add(image_panel, BorderLayout.WEST);
         this.add(info_panel, BorderLayout.CENTER);
     }
-
     public void updateView() {
 
-    }
-
-    /*
-    // used for scaling image
-    private static BufferedImage resize(BufferedImage img, int height, int width) {
-        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = resized.createGraphics();
-        g2d.drawImage(tmp, 0, 0, null);
-        g2d.dispose();
-        return resized;
-    }
-
-     */
-
-    // used for scaling image
-    public static Dimension getScaledDimension(Dimension originalDim, Dimension boundary) {
-        int newHeight = originalDim.height;
-        int newWidth = originalDim.width;
-
-        if (newHeight > boundary.height) {
-            newHeight = boundary.height;
-            newWidth = (newHeight * originalDim.width) / originalDim.height;
-        }
-
-        if (originalDim.width > boundary.width) {
-            newWidth = boundary.width;
-            newHeight = (newWidth * originalDim.height) / originalDim.width;
-        }
-        return new Dimension(newWidth, newHeight);
     }
 }
